9cfa54f7e537d21de6de86d7ee3eead4c4e72a0b
Seyfullah <seyanardag@gmail.com>
2025-06-13 17:19:26 +0300

    serviceworker path bug fix

--- sw.js
+++ sw.js
@@ -1,16 +1,15 @@
 const CACHE_NAME = 'radio-cache-v1';
 const urlsToCache = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/favicon.ico',
-    '/favicon.svg',
-    '/favicon-96x96.png',
-    '/apple-touch-icon.png',
-    '/web-app-manifest-192x192.png',
-    '/web-app-manifest-512x512.png',
-    '/site.webmanifest'
+    'index.html',
+    'style.css',
+    'script.js',
+    'favicon.ico',
+    'favicon.svg',
+    'favicon-96x96.png',
+    'apple-touch-icon.png',
+    'web-app-manifest-192x192.png',
+    'web-app-manifest-512x512.png',
+    'site.webmanifest'
 ];
 
 self.addEventListener('install', event => {
